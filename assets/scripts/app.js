0d9087932adea051a4d1bbb83583922572bb1f60
JohanWerther <fedelabandeira490@gmail.com>
2022-06-15 00:17:28 -0300

    finalizado condicional del final de batalla en => attackHandler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,7 @@
-const ATTACK_VALUE = 10; // We use this typo bc is a global value that isn't going to change
+const ATTACK_VALUE = 40; // We use this typo bc is a global value that isn't going to change
 const MONSTER_ATTACK_VALUE = 14;
 
-let chosenMaxLife = 100;
+let chosenMaxLife = 5;
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 
@@ -14,7 +14,17 @@ function attackHandler() {
     currentPlayerHealth -= playerReceivedDamage;
 
     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0 ) {
-        alert('You slay the monster!');
+        setTimeout(() => {
+            alert('You slay the monster!');
+        },500);
+    } else if (currentMonsterHealth > 0 && currentPlayerHealth <= 0) {
+        setTimeout(() => {
+            alert('You have been slain!')
+        },500)
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        setTimeout(() => {
+            alert('Next round');
+        },500);
     }
 }
 
